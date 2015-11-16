28f5dcd94d261ce9a6b278ac2ed90d9f80391e31
VladoKundrat <kundratvlado@gmail.com>
2015-11-16 11:07:58 +0100

    render Albums  edit piece

--- database_of_janacek_works/urls.py
+++ database_of_janacek_works/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, patterns
 from . import views
-from database_of_janacek_works.views import PieceListRenderer
+from database_of_janacek_works.views import PieceListRenderer, AlbumListRenderer
 
 urlpatterns = patterns('',
     url(r'^$', views.index),
     url(r'^piece/$',PieceListRenderer.as_view(), name='piece_list'),
- #   url(r'^album/$',AlbumListRenderer.as_view()),
+    url(r'^album/$',AlbumListRenderer.as_view(), name='album_list'),
     #url(r'^rest/piece/$',views.PieceListJson.as_view(), name='rest_piece_data'),
 
 )
--- database_of_janacek_works/views.py
+++ database_of_janacek_works/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 from database_of_janacek_works.static.datatableview.views import DatatableView
-from database_of_janacek_works.models import Piece
+from database_of_janacek_works.models import Piece, Album
 
 
 def index(request):
@@ -11,3 +11,26 @@ class PieceListRenderer(DatatableView):
 
     # The model we're going to show
     model = Piece
+
+    datatable_options = {
+        'columns': [
+            'piece_id',
+            'cz_title',
+            'en_title',
+            ("catalogue number", ['jw_catalogue_number_part_I', 'jw_catalogue_number_part_II'], 'get_catalogue_number'),
+            'time_of_composition',
+            'date_of_premiere',
+            'place_of_premiere',
+            'compose_for',
+            'number_of_movement',
+            'wiki'
+        ],
+        'hidden_columns': ['piece_id', 'compose_for', 'number_of_movement', 'wiki'],
+    }
+
+    def get_catalogue_number(self, instance, *args, **kwargs):
+        return "%s / %s" % (instance.jw_catalogue_number_part_I, instance.jw_catalogue_number_part_II)
+
+class AlbumListRenderer(DatatableView):
+
+    model = Album
